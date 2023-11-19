cb6b2e3fd38d748801a8f9e5d343b80bffefb5e2
LarSyMain <96264278+LarSyMain@users.noreply.github.com>
2023-11-19 18:25:43 +0300

    Update data.py

--- data.py
+++ data.py
@@ -9,10 +9,15 @@ def load_recipes():
             id = f.readline().strip()
             name = f.readline().strip()
             description = ""
+            eatingtime = f.readline().strip()
+            foodtype = f.readline().strip()
+            cookingtime = f.readline().strip()
+            coastfood = f.readline().strip()
+            foodmass = f.readline().strip()
             for line in f.readlines():
                 description += line
 
-            rec = Recipe(id, name, description)
+            rec = Recipe(id, name, description, eatingtime, foodtype, cookingtime, coastfood, foodmass)
             result_recipes.append(rec)
             f.close()
     print(f"Loaded {len(result_recipes)} recipes")
